39169967495460716d6198b1cfad5ae5eb9bad58
Jstngr <justi93smail@gmail.com>
2024-03-03 19:04:32 +0100

    remove from other repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
 import React from 'react';
 import Header from 'components/header';
-import Portrait from 'assets/portrait.png';
 import './App.css';
 
 export default function App() {
   return (
     <>
       <Header />
-      {/* <nav>nav</nav>
-      <aside>aside</aside> */}
       <main>
         <section>
-          <h2>Hey, im Tobi</h2>
-          <h1>Lead Engineer</h1>
-          <p>
-            at <a href="https://sosafe-awareness.com/">SoSafe</a>
-          </p>
-        </section>
-
-        <section className="PortraitSection">
-          <img src={Portrait} alt="My" className="PortraitSection-portrait"></img>
-          <img src={Portrait} alt="My" className="PortraitSection-portrait background"></img>
+          <h1>MiniGenie</h1>
         </section>
       </main>
-      {/* <footer>footer</footer>  */}
     </>
   );
 }
